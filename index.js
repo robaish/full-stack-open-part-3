08b10fad9fab7b62ae08c1cbf15f956a283b7c1c
Robert Nuorteva <roba@hey.com>
2021-11-11 11:43:29 +0200

    Phonebook backend step 3

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // import express
+const { response } = require('express');
 const express = require('express');
 // set instance of express
 const app = express();
@@ -26,15 +27,24 @@ let persons = [
   }
 ];
 
-// define routes
+// define root route
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook</h1>')
 });
 
+// get all persons
 app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+// get single person
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(p => p.id === id);
+  person ? response.json(person) : response.status(404).end();
+})
+
+// define info route
 app.get('/info', (request, response) => {
   const responseTime = new Date();
   response.write(`<p>Phonebook has info for ${persons.length} people</p>`);
