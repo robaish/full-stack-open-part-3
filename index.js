2c4feae32ba4a76979a055f5b17adbfe53d5aefb
Robert Nuorteva <roba@hey.com>
2021-11-15 20:03:15 +0200

    Exercise 3.14: new numbers saved to database

--- index.js
+++ index.js
@@ -24,29 +24,6 @@ app.use(cors());
 // activate static
 app.use(express.static('build'));
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-];
-
 // define root route
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook</h1>')
@@ -61,10 +38,15 @@ app.get('/api/persons', (request, response) => {
 
 // get single person
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(p => p.id === id);
-  person ? response.json(person) : response.status(404).send('That person does not exist.').end();
-})
+  Person.findById(request.params.id)
+  .then(person => {
+    response.json(person);
+  })
+  .catch(error => {
+    console.log(error)
+    response.status(404).send('That person is not in the phonebook.').end()
+  })
+});
 
 // define info route
 app.get('/info', (request, response) => {
@@ -76,23 +58,22 @@ app.get('/info', (request, response) => {
 
 // add person
 app.post('/api/persons/', (request, response) => {
+  const body = request.body;
 
-  const person = request.body;
-
-  if (!person.name || !person.number) {
+  if (body.name === "" || body.number === "") {
     return response.status(400).json({
       error: 'Name or number is missing.'
     });
   }
 
-  if (persons.find(p => p.name === person.name)) {
-    return response.status(409).json({
-      error: 'The name of this person is already added.'
-    })
-  }
-  person.id = Math.floor(Math.random()*1000000);
-  persons = persons.concat(person);
-  response.json(person);
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  });
 })
 
 // delete person
