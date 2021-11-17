dc38034624e8c329a284ec966245993e60a5ac4b
Robert Nuorteva <roba@hey.com>
2021-11-17 18:33:16 +0200

    3.19 phonebook database, step 7 - unique name validation

--- index.js
+++ index.js
@@ -72,33 +72,31 @@ app.get('/info', (request, response) => {
 });
 
 // add person
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
   const body = request.body;
-
-  if (body.name === "" || body.number === "") {
-    return response.status(400).json({
-      error: 'Name or number is missing.'
-    });
-  }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  });
-})
+  person
+    .save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+      response.json(savedAndFormattedPerson);
+    })
+    .catch(error => next(error));
+});
 
 // delete person
 app.delete('/api/persons/:id', (request, response, next) => {
   Person
-  .findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end();
-  })
-  .catch(error => next(error));
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 // update person
@@ -111,11 +109,11 @@ app.put('/api/persons/:id', (request, response, next) => {
   }
   
   Person
-  .findByIdAndUpdate(request.params.id, person, {new: true})
-  .then(updatedPerson => {
-    response.json(updatedPerson);
-  })
-  .catch(error => next(error));
+    .findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson);
+    })
+    .catch(error => next(error));
 });
 
 // handle requests with unknown endpoint
@@ -131,6 +129,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 }
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI;
 
@@ -14,9 +15,11 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: { type: String, unique: true },
+  number: { type: String }
 });
+
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
