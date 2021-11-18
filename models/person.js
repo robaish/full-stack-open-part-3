09b1436f8c294d43b7795e19d0e0cc4965f264cb
Robert Nuorteva <roba@hey.com>
2021-11-18 18:46:15 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URI;
 console.log('connecting to', url);
 
 mongoose
-  .connect(url)
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
     console.log('connected to MongoDB');
   })
