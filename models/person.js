9b6c76aa93d848f904e76e3a741c450b5360bbeb
Robert Nuorteva <roba@hey.com>
2021-11-18 18:55:12 +0200

    Database connect modification

--- models/person.js
+++ models/person.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URI;
 console.log('connecting to', url);
 
 mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(url)
   .then(result => {
     console.log('connected to MongoDB');
   })
